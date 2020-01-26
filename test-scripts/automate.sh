82cfb32d1083026334b24065f8b05277fa09897f
Gary Vermeulen <gary.vermeulen@broadcom.com>
2020-01-26 16:13:26 +0000

    test

--- test-scripts/automate.sh
+++ test-scripts/automate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./makeLicense.sh -namespace=test -dest=../releases/test/gateway-license.yaml -license=../licenses/license.xml
-./makeImageCreds.sh -namespace=test -dest=../releases/test/image-creds.yaml -username=gv666792 -password=CAdemo123!
+./makeImageCreds.sh -namespace=test -dest=../releases/test/image-creds.yaml -password=CAdemo123!
 ./makeEnv.sh -namespace=test -dest=../releases/test/env.yaml -ext=yaml
